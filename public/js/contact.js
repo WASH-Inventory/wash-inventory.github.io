e27b4d5f1a1b8392fa1baf730c4ddd245198caf1
WASH-Inventory <108815696+WASH-Inventory@users.noreply.github.com>
2023-05-03 17:07:58 +0300

    Add files via upload

---
+++ public/js/contact.js
@@ -0,0 +1,39 @@
+const inputs = document.querySelectorAll(".input");
+var btn = document.getElementById('btn')
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+///////////////////////
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = '';
+   const templateID = '';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send';
+      alert("📧Message Adressed Succesfully !!!📧");
+    }, (err) => {
+      btn.value = 'Send';
+      alert(JSON.stringify(err));
+    });
+});
